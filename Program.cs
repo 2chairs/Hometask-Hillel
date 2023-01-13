43094b4daba3fd6b68b4b01655ced5ce17ce9a33
KOMPUTER-YUR\valera <valeriikaira@gmail.com>
2023-01-13 14:18:32 +0200

    рядок 54 виключення

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, try calculator \nyou can try to do some math operation like:\n+ , -, *, /, & (raise to a power) etc \nyou also can use () paranthesis\n");
-            var tokens = Tokenizer.Tokenize("5.2+2&3-7");
+            var tokens = Tokenizer.Tokenize("5.2+2&3-7"); // komment
             var result = Calculator.Calculate(tokens);
             result.ToString();
 
