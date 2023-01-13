44775aac4f4bf96bf09159fb89a6c22f8a5c060a
KOMPUTER-YUR\valera <valeriikaira@gmail.com>
2023-01-13 14:43:54 +0200

    in Tokinizer class

--- Program.cs
+++ Program.cs
@@ -5,10 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, try calculator \nyou can try to do some math operation like:\n+ , -, *, /, & (raise to a power) etc \nyou also can use () paranthesis\n");
-            var tokens = Tokenizer.Tokenize("5.2+2&3-7"); // komment
+            var tokens = Tokenizer.Tokenize("5.2+2&3-7"); 
             var result = Calculator.Calculate(tokens);
             result.ToString();
-
+            // line 54 in Tokinizer class - unknown for me exception
         }
     }
 }
